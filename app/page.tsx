c8378541e170f58c1759a256f4856453be693bfa
Azaiez Nourhene <nourazaiez2001@gmail.com>
2025-01-25 16:03:01 +0100

    fixed Hero bg

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-centre flex-col overflow-hidden mx-auto sm:px-10 px-5" >
+    <main className="relative bg-black-100 flex justify-center items-centre flex-col overflow-hidden mx-auto" >
       <div className="max-w-7xl w-full">
         <Hero />
       </div>
