da7f10758ed617395ef1ea4f8e789cfc146a21cc
Azaiez Nourhene <nourazaiez2001@gmail.com>
2025-01-29 08:40:17 +0100

    Form template done

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { ColourfulText } from './ui/colourful-text';
+import { ContactForm } from './ui/contactform';
 
 interface ProjectProps {
     id?: string;
@@ -6,8 +8,15 @@ interface ProjectProps {
 
 const Contact: React.FC<ProjectProps> = ({ id }) => {
   return (
-    <div id={id} className='dark:bg-zinc-900 bg-zinc-50 dark:bg-grid-white/[0.02] bg-grid-black/[0.05]' >
-        Contact
+    <div id={id} className='dark:bg-zinc-900 bg-zinc-50 dark:bg-grid-white/[0.02] bg-grid-black/[0.05] pb-20' >
+        <div className="flex flex-col px-0 sm:px-14 lg:px-18 text-center items-center justify-center">
+          <p className="font-bold text-2xl mx-8 dark:text-white pt-7 mb-1"><ColourfulText text="Contact" /></p>
+          <p className="font-extrabold text-center text-4xl mt-2 mx-8 dark:text-white">Strong connections drive meaningful opportunities</p>
+          <p className="font-normal text-sm mx-8 dark:text-white mb-10 mt-7">
+            Feel free to reach out for collaborations, opportunities, or discussions on technology and innovation.
+          </p>
+        </div>
+        <ContactForm />
     </div>
   );
 }
