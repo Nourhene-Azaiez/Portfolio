1a53c6dd74e2380e488770e7f52eb8fa075dc52b
Azaiez Nourhene <nourazaiez2001@gmail.com>
2025-01-29 09:05:04 +0100

    contact front complete

--- components/ui/contactform.tsx
+++ components/ui/contactform.tsx
@@ -1,5 +1,6 @@
 "use client";
-import React from "react";
+import emailjs from "emailjs-com";
+import React, { useState } from "react";
 import { Label } from "../ui/label";
 import { Input, Textarea } from "../ui/input";
 import { cn } from "@/lib/utils";
@@ -10,13 +11,39 @@ import {
   } from "@tabler/icons-react";
 
 export function ContactForm() {
+  
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("Message sent");
+  
+    // Get form data
+    const formData = new FormData(e.target as HTMLFormElement);
+    const firstName = formData.get("firstname") as string;
+    const lastName = formData.get("lastname") as string;
+    const email = formData.get("email") as string;
+    const message = formData.get("message") as string;
+  
+    const templateParams = {
+      firstname: firstName,
+      lastname: lastName,
+      email: email,
+      message: message,
+    };
+  
+    // Send the email using EmailJS
+    emailjs
+      .send("your_service_id", "your_template_id", templateParams, "your_user_id")
+      .then(
+        (response) => {
+          console.log("Message sent successfully:", response);
+        },
+        (error) => {
+          console.error("Error sending message:", error);
+        }
+      );
   };
 
   return (
-    <div className="max-w-5xl w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-zinc-300 dark:bg-black">
+    <div className="max-w-6xl w-full lg:mx-auto mx-5 rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-zinc-300 dark:bg-black">
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
         Contact Me
       </h2>
@@ -42,7 +69,7 @@ export function ContactForm() {
                 <Input id="email" placeholder="you@example.com" type="email" required />
                 </LabelInputContainer>
             </div>
-            <LabelInputContainer className="max-h-full h-full">
+            <LabelInputContainer className="h-full">
                 <Label htmlFor="message">Message</Label>
                 <Textarea id="message" placeholder="Your message..." rows={4} required />
             </LabelInputContainer>
@@ -57,8 +84,9 @@ export function ContactForm() {
             </button>
             <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-2">
                 <button
-                    className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-full h-10 font-medium shadow-input bg-gray-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-                    type="submit"
+                    className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-full h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+                    onClick={() => window.open("https://github.com/Nourhene-Azaiez", "_blank")}
+
                 >
                     <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
                     <span className="text-neutral-700 dark:text-neutral-300 text-sm">
@@ -68,7 +96,7 @@ export function ContactForm() {
                 </button>
                 <button
                     className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-full h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-                    type="submit"
+                    onClick={() => window.open("https://www.linkedin.com/in/nourhene-azaiez/", "_blank")}
                 >
                     <IconBrandLinkedin className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
                     <span className="text-neutral-700 dark:text-neutral-300 text-sm">
@@ -78,7 +106,7 @@ export function ContactForm() {
                 </button>
                 <button
                     className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-full h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-                    type="submit"
+                    onClick={() => window.open("https://maps.app.goo.gl/8UnEd21n6AqhuKAU8", "_blank")}
                 >
                     <IconLocation className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
                     <span className="text-neutral-700 dark:text-neutral-300 text-sm">
@@ -88,8 +116,6 @@ export function ContactForm() {
                 </button>
             </div>
         </div>
-        
-        <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
       </form>
     </div>
   );
